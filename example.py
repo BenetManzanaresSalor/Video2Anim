d353ced7c77fe9bb130cb5be98021ce059326258
BenetManzanaresSalor <benetmanzanares@gmail.com>
2023-03-12 09:48:18 +0100

    Minor example.py fixing

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ from video2anim import Video2Anim
 if __name__ == "__main__":
 	# Required settings
 	video_path = "Body.mp4"
-	anim_path = "Animations"
+	output_folder = "Animations"
 	openpose_path = "OpenPose"
 	bones_defs = [[8, 1, -1, 'bone_1/bone_2'],  # Backbone
 	              [1, 0, 0, 'bone_1/bone_2/bone_3'],  # Head
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 	avg_keys_per_second = 0
 
 	# Creation
-	v2a = Video2Anim(video_path=video_path, anim_path=anim_path, openpose_path=openpose_path,
+	v2a = Video2Anim(video_path=video_path, output_folder=output_folder, openpose_path=openpose_path,
 	                 bones_defs=bones_defs, body_orientation=body_orientation,
 	                 min_confidence=min_confidence, min_trembling_freq=min_trembling_frequency,
 	                 mlf_max_error_ratio=mlf_max_error_ratio, avg_keys_per_sec=avg_keys_per_second)
